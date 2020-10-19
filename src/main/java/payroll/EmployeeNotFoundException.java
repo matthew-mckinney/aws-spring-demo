0b96e3f9de74be0f646a6c3f139333492b9792c6
Matt McKinney <smattmckinney@gmail.com>
2020-10-19 15:50:55 -0500

    initial commit

---
+++ src/main/java/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
